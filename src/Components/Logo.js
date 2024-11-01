7d70c686ee2bbee5389bbed52e9199f33fd7bb65
dadafivethousand <maxli5004@gmail.com>
2024-11-01 16:49:33 -0400

    Initial commit

---
+++ src/Components/Logo.js
@@ -0,0 +1,17 @@
+import '../Stylesheets/Logo.css'
+export default function Logo(){
+    return(
+<div class="logo-container">
+    <div className='outer-box'>
+  <div class="box">
+    <div className='k'>
+5K
+    </div>
+    <div class="text">Studios</div>
+  </div>
+  </div>
+  
+  <div class="subtext">Web Development</div>
+</div>
+)
+}
